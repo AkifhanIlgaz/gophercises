120c456c8755bfe2deb2f92d7f390e3cde8d885d
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-04-15 03:12:24 +0300

    implement parsing links

--- link-parser/main.go
+++ link-parser/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"os"
+
+	"github.com/AkifhanIlgaz/gophercises/link-parser/parser"
 )
 
 func main() {
@@ -11,23 +15,37 @@ func main() {
 		TODO:
 		1. Accept multiple files
 		2. Accept URLS
-		3. Add testing for edge cases
+		3. Add testing for edge cases // Testing for parsing a website
+		4. Create a new struct with filename and links
 	*/
 
-	files := os.Args[1:]
+	for _, source := range os.Args[1:] {
+		var sourceReader io.Reader
 
-	_, err := url.ParseRequestURI(files[0])
-	if err != nil {
-		fmt.Println(err)
+		if isValidUrl(source) {
+			resp, err := http.Get(source)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer resp.Body.Close()
+			sourceReader = resp.Body
+		} else {
+			f, err := os.Open(source)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer f.Close()
+			sourceReader = f
+		}
+
+		links := parser.Parse(sourceReader)
+		fmt.Printf("%+v\n", links)
 	}
-	
-	// f, err := os.Open(files[0])
-	// if err != nil {
-	// 	fmt.Errorf("unable to open file: %v", err)
-	// }
 
-	// links := parser.Parse(f)
+}
 
-	// fmt.Printf("%+v", links)
+func isValidUrl(input string) bool {
+	_, err := url.ParseRequestURI(input)
 
+	return err == nil
 }
