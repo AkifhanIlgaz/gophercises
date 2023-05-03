200efb2e1af9886ce4d0166175e8ae500058efae
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-03 13:05:55 +0300

    use cobra-cli init

--- task/main.go
+++ task/main.go
@@ -1,10 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
 import "github.com/AkifhanIlgaz/gophercises/task/cmd"
 
-//    ~/.bashrc
-//export PATH="~/go/bin:$PATH"
-
 func main() {
-	cmd.RootCommand.Execute()
+	cmd.Execute()
 }
