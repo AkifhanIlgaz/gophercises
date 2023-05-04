228d19bb0a5e161f10c8232f7f275fa0b08baff9
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-04 19:12:31 +0300

    add command

---
+++ task/cmd/add.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Adds a task to your task list",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("task added")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
