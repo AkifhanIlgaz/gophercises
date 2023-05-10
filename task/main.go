9f3ac77ac5a5b6531502dde13c58edf8317220b1
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-10 14:54:44 +0300

    create tasks bucket

--- task/main.go
+++ task/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/AkifhanIlgaz/gophercises/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/AkifhanIlgaz/gophercises/task/cmd"
+	"github.com/AkifhanIlgaz/gophercises/task/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+
 	cmd.Execute()
+}
+
+// Helper function to handle errors
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
