1066281fc46ef3b8df2ecc1b2d54e3879fcf9e10
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-10 15:39:07 +0300

    implement add functionality

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/AkifhanIlgaz/gophercises/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +13,13 @@ var addCmd = &cobra.Command{
 	Short: "Adds a task to your task list",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Printf("Somethinh went wrong while adding \"%s\" to your task list: %s \n", task, err.Error())
+			return
+		}
+
 		fmt.Printf("Added \"%s\" to your task list \n", task)
 	},
 }
